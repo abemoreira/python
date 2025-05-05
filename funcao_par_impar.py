6ed45694c66d942a5763e66868b0604caef79ee0
abemoreira <abesam200319@gmail.com>
2025-05-05 16:46:56 -0400

    Add files via upload

---
+++ funcao_par_impar.py
@@ -0,0 +1,16 @@
+def par_ou_impar():
+    
+    num = int(input("digite um numero: "))
+    
+    if num % 2 == 0:
+        print(f"{num} é um numero par")
+    else:
+        print(f"{num} é um numero impar")
+        
+        return par_ou_impar
+    
+
+num2= par_ou_impar()
+
+    
+    
